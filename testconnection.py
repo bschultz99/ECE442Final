e2e662cd95d4cfd128c82d2cd32d5dd4027499d0
Bryant <bryantschultz99@gmail.com>
2022-04-17 09:51:22 -0500

    Update testconnection.py

--- testconnection.py
+++ testconnection.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
 
     while True:
-        ser.write(b"Hello from Raspberry Pi!\n")
+        ser.write(b"100,0,255,255,1\n")
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
