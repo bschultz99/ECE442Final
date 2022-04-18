c76b8f6c63cecfee5cbb50df52339c25ee75d655
Bryant <bryantschultz99@gmail.com>
2022-04-18 14:03:34 -0500

    Update testconnection.py

--- testconnection.py
+++ testconnection.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
         thingspeak_field2 = {"field2": red}
         thingspeak_field3 = {"field3": green}
         thingspeak_field4 = {"field4": blue}
-        #thingspeak_field5 = {"field5": line.split(",")[2]}
         thingspeak_field6 = {"field6": servo}
-        #thingspeak_field7 = {"field7": line.split(",")[0]}
-        #thingspeak_field8 = {"field8": line.split(",")[1]}
-        print(len(line.split(",")))
+        if (len(line.split(",")) == 3):
+            thingspeak_field5 = {"field5": line.split(",")[2]}
+            thingspeak_field7 = {"field7": line.split(",")[0]}
+            thingspeak_field8 = {"field8": line.split(",")[1]}
         time.sleep(1)
