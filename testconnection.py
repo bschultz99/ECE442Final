ca8d4c61b9652de36d99492660451bea5d1dd5ec
Bryant <bryantschultz99@gmail.com>
2022-04-18 09:25:08 -0500

    Update testconnection.py

--- testconnection.py
+++ testconnection.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
     ser.reset_input_buffer()
     channel_id = 1707389
-    key = B4XFISTMGQ65143C
+    key = "B4XFISTMGQ65143C"
     url = 'https://api.thingspeak.com/update'
     ts = thingspeak.Channel(channel_id, key, url)
     data = ""
