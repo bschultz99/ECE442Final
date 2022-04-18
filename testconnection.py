cf0c1e04b6d2b65a5f49a57798ed7dafd6c1636e
Bryant <bryantschultz99@gmail.com>
2022-04-18 13:38:38 -0500

    Update testconnection.py

--- testconnection.py
+++ testconnection.py
@@ -11,12 +11,25 @@ if __name__ == '__main__':
     url = 'https://api.thingspeak.com/update'
     ts = thingspeak.Channel(channel_id, key, url)
     data = ""
+    fans = 100
+    red = 0
+    green = 255
+    blue = 255
+    servo = 0
     while True:
-        ser.write(b"100\n")
-        ser.write(b"0\n")
-        ser.write(b"255\n")
-        ser.write(b"255\n")
-        ser.write(b"1\n")
+        ser.write( fans + b"\n")
+        ser.write(red + b"\n")
+        ser.write(green + b"\n")
+        ser.write(blue + b"\n")
+        ser.write(servo + b"\n")
         line = ser.readline().decode('utf-8').rstrip()
+        thingspeak_field1 = {"field1": fans}
+        thingspeak_field2 = {"field2": red}
+        thingspeak_field3 = {"field3": green}
+        thingspeak_field4 = {"field4": blue}
+        thingspeak_field5 = {"field5": line.split(",")[2]}
+        thingspeak_field6 = {"field6": servo}
+        thingspeak_field7 = {"field7": line.split(",")[0]}
+        thingspeak_field8 = {"field8": line.split(",")[1]}
         print(line.split(",")[0])
         time.sleep(1)
