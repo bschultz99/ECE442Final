486d96d5c0db8948a8089aa7e88d39f4ee95ccea
Bryant <bryantschultz99@gmail.com>
2022-04-18 09:27:50 -0500

    Update testconnection.py

--- testconnection.py
+++ testconnection.py
@@ -18,6 +18,5 @@ if __name__ == '__main__':
         ser.write(b"255\n")
         ser.write(b"1\n")
         line = ser.readline().decode('utf-8').rstrip()
-        data.split(",")
-        print(data)
+        print(line.type)
         time.sleep(1)
