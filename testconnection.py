e6881569d4c4c2ee3280850dd3008ab6cac41ead
Bryant <bryantschultz99@gmail.com>
2022-04-18 13:51:08 -0500

    Update testconnection.py

--- testconnection.py
+++ testconnection.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         thingspeak_field2 = {"field2": red}
         thingspeak_field3 = {"field3": green}
         thingspeak_field4 = {"field4": blue}
-        thingspeak_field5 = {"field5": line.split(",")[2]}
+        #thingspeak_field5 = {"field5": line.split(",")[2]}
         thingspeak_field6 = {"field6": servo}
         thingspeak_field7 = {"field7": line.split(",")[0]}
         thingspeak_field8 = {"field8": line.split(",")[1]}
